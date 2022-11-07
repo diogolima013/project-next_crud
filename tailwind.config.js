faadf6455a308ab7bc1438bd63c6b9c35477e693
Diogo <diogoslva0133@gmail.com>
2022-11-07 09:33:43 -0300

    upload the files

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+  purge: [
+  
+  "./src/pages/**/*.{js,ts,jsx,tsx}",
+  
+  "./src/components/**/*.{js,ts,jsx,tsx}",
+  
+  ],
+  
+  theme: {
+  
+  extend: {},
+  
+  },
+  
+  plugins: [],
+  
+  }
