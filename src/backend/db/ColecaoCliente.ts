925cb1890f30a82a4b840ac64229c69bec95d2e9
Diogo <diogoslva0133@gmail.com>
2022-11-08 11:34:11 -0300

    Organizing Code with Hooks

--- src/backend/db/ColecaoCliente.ts
+++ src/backend/db/ColecaoCliente.ts
@@ -16,7 +16,7 @@ export default class ColecaoCliente implements ClienteRepositorio {
             return new Cliente(dados.nome, dados.idade, snapshot.id)
         }
     }
-
+    
     async salvar(cliente: Cliente): Promise<Cliente> {
         if(cliente?.id) {
             await this.colecao().doc(cliente.id).set(cliente)
@@ -26,21 +26,20 @@ export default class ColecaoCliente implements ClienteRepositorio {
             const doc = await docRef.get()
             return doc.data()
         }
-}
+    }
+
     async excluir(cliente: Cliente): Promise<void> {
-        return this.colecao()
-        .doc(cliente.id)
-        .delete()
-}
+        return this.colecao().doc(cliente.id).delete()
+    }
+
     async obterTodos(): Promise<Cliente[]> {
-       const query = await this.colecao().get()
-       return query.docs.map((doc: { data: () => any; }) => doc.data())
-}
+        const query = await this.colecao().get()
+        return query.docs.map(doc => doc.data()) ?? []
+    }
 
     private colecao() {
         return firebase
-        .firestore().collection('clientes')
-        .withConverter(this.#conversor)
+            .firestore().collection('clientes')
+            .withConverter(this.#conversor)
     }
-
 }
